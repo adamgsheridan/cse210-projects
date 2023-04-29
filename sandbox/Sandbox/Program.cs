b59237aee414dcd5c164d57532c77f4af6845165
adamgsheridan <adamgsheridan7@gmail.com>
2023-04-29 12:56:37 -0600

    Finished these things from week 2 as well

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World! I'm Adam!");
+        //Gets the user's first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        //Gets the user's last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        //Combines the above first and last name
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
