a67497fd7c0ce361206c31efa008bdbc7aedbd3b
adamgsheridan <adamgsheridan7@gmail.com>
2023-05-11 14:27:49 -0600

    Been a bit

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -7,3 +7,14 @@ class Program
         Console.WriteLine("Hello Develop02 World!");
     }
 }
+
+
+class Journal {
+    public List<Entry> _entries = new List<Entry>();
+}
+
+class Entry {
+    public string _text;
+    public string _prompt;
+    public string _date;
+}
