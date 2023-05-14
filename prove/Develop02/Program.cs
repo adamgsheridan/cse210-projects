813abc8eb4e21b45b9f654e21d052ca1e1658f22
adamgsheridan <adamgsheridan7@gmail.com>
2023-05-13 22:17:43 -0600

    Heres what I got

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,18 +1,20 @@
 using System;
 
-class Program
-{
+class Program {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        //Who was the most interesting person I interacted with today?", "What was the best part of my day?", 
+        //"How did I see the hand of the Lord in my life today?", "What was the strongest emotion I felt today?",
+        // "If I had one thing I could do over today, what would it be?
+        Console.WriteLine("Welcome to the Journal Program!");
+        Console.WriteLine("Please select one of the following choices:\n1. Write\n2. Display\n3. Load\n4. Save\n5. Quit");
+        Console.Write("What would you like to do? ");
+        string choice = Console.ReadLine();
     }
 }
-
-
 class Journal {
     public List<Entry> _entries = new List<Entry>();
 }
-
 class Entry {
     public string _text;
     public string _prompt;
--- prove/Develop02/text.cs
+++
@@ -1,9 +0,0 @@
-using System;
-public class Program
-{
-  static void Main(string[] args)
-  {
-    PromptGenerator menu = new PromptGenerator();
-    Console.WriteLine(menu.welcome);
-    }
-}
