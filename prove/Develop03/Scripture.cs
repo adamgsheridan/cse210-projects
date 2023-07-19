f54cc0c122d5efa6c14af9519abb8654e2098654
adamgsheridan <adamgsheridan7@gmail.com>
2023-07-18 20:56:44 -0600

    finished

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,33 +1,22 @@
 public class Scripture
 {
+    public string GetReference()
+    {
+        string reference = "John 3:16-17";
+        return reference;
+    }
     public string GetScripture()
     {
         string scripture = "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life \nFor God sent not his Son into the world to condemn the world; but that the world through him might be saved.";
         return scripture;
     }
 
-    //public List<string> SortScripture()
-    void SortScripture()
+    public string[] SortScripture()
     {
         Scripture OriginalScripture = new Scripture();
         string scripture = OriginalScripture.GetScripture();
-
-        //List<String> SortedScripture = new List<String>();
-        //SortedScripture.Add("hello");
-        //Console.WriteLine(SortedScripture);
-        //int counter = 0;
-        //for (int i = 0; i <= scripture.Length; i++)
-        //{
-            //SortedScripture.Add(i);
-        //}
-        //foreach(var i in scripture.Split(" "))
-        //{
-            //Console.WriteLine(i);
-        //    SortedScripture.Add(i);
-            //Console.WriteLine(SortedScripture);
-            //counter++;
-        //}
-        //return SortedScripture;
+        string[] SortedScriptures = scripture.Split(" ");
+        return SortedScriptures;
     }
     
     //string[] scriptureReference = {"John 3:16-17"};
--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,8 +1,39 @@
 class Word {
-    public string GetWord()
+    public int GetRandomNumber()
     {
-            string word = "";
-            return word;
+        Scripture scripture = new Scripture();
+        string[] script = scripture.SortScripture();
+        Random rnd = new Random();
+        int number = rnd.Next(script.Length);
+        return number;
+    }
+    public string MakeHidden()
+    {
+        Scripture scripture = new Scripture();
+        string[] WholeScripture = scripture.SortScripture();
+        Word word = new Word();
+        int RandomNumber = word.GetRandomNumber();
+        String[] HiddenWord = new String[20];
+        int counter = 0;
+        for (int i = 0; i <= RandomNumber; i++)
+        {
+            if (i == RandomNumber)
+            {
+                //Console.Write(WholeScripture[i]);
+                foreach (var whole in WholeScripture[i])
+                {
+                    counter++;
+                }
+            }
+        }
+        //Console.Write(counter);
+        string letters = "";
+        for (int i = 0; i < counter; i++)
+        {
+            letters = letters + ("_");
+        }
+        //Console.Write(letters);
+        return letters;
     }
 
 }
